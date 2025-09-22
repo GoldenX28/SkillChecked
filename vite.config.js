f8781eae81e2973be8966f22ee50c99f05187586
Kristians Sapožņikovs <119543405+GoldenX28@users.noreply.github.com>
2025-09-22 12:23:01 +0000

    added home page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',   // allow external access (not just [::1])
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',  // in Codespaces, you may need to replace with your forwarded URL
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -18,3 +26,4 @@ export default defineConfig({
         }),
     ],
 });
+
